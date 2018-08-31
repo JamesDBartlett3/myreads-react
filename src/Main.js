fc44b91a1022d046587b4efdf40fa802ca3513a0
James D. Bartlett III <jimmyb@gmail.com>
2018-08-31 00:14:09 -0500

    Shelf component implemented and functioning properly.

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-//import Shelf from './Shelf'
-import Book from './Book'
+import Shelf from './Shelf'
 
 class Main extends Component {
 	render() {
@@ -12,60 +11,24 @@ class Main extends Component {
               </div>
               <div className="list-books-content">
                 <div>
-                  <div className="bookshelf">
-                    <h2 className="bookshelf-title">Currently Reading</h2>
-                    <div className="bookshelf-books">
-                      <ol className="books-grid">
-						  {this.props.books
-						  	.filter(book => book.shelf === 'currentlyReading')
-							.map(book => (
-								<li key={book.id}>
-									<Book
-										book={book}
-									  	changeShelf={this.props.changeShelf}
-									  	currentShelf='currentlyReading'
-									/>
-		                        </li>
-							))}
-                      </ol>
-                    </div>
-                  </div>
-                  <div className="bookshelf">
-                    <h2 className="bookshelf-title">Want to Read</h2>
-                    <div className="bookshelf-books">
-                      <ol className="books-grid">
-						  {this.props.books
-						  	.filter(book => book.shelf === 'wantToRead')
-							.map(book => (
-								<li key={book.id}>
-									<Book
-										book={book}
-									  	changeShelf={this.props.changeShelf}
-									  	currentShelf='wantToRead'
-									/>
-		                        </li>
-							))}
-                      </ol>
-                    </div>
-                  </div>
-                  <div className="bookshelf">
-                    <h2 className="bookshelf-title">Read</h2>
-                    <div className="bookshelf-books">
-                      <ol className="books-grid">
-						{this.props.books
-						  .filter(book => book.shelf === 'read')
-						  .map(book => (
-							  <li key={book.id}>
-								  <Book
-									  book={book}
-									  changeShelf={this.props.changeShelf}
-									  currentShelf='read'
-								  />
-							  </li>
-						  ))}
-                      </ol>
-                    </div>
-                  </div>
+					<Shelf
+						shelfName='currentlyReading'
+						innerText='Currently Reading'
+						books={this.props.books}
+						changeShelf={this.props.changeShelf}
+					/>
+					<Shelf
+						shelfName='wantToRead'
+						innerText='Want To Read'
+						books={this.props.books}
+						changeShelf={this.props.changeShelf}
+					/>
+					<Shelf
+						shelfName='read'
+						innerText='Read'
+						books={this.props.books}
+						changeShelf={this.props.changeShelf}
+					/>
                 </div>
               </div>
               <div className="open-search">
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,10 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
+import Book from './Book'
 
 class Shelf extends Component {
 	render() {
 		return(
-			<div></div>
+			<div className="bookshelf">
+			  <h2 className="bookshelf-title">{this.props.innerText}</h2>
+			  <div className="bookshelf-books">
+				<ol className="books-grid">
+					{this.props.books
+					  .filter(book => book.shelf === this.props.shelfName)
+					  .map(book => (
+						  <li key={book.id}>
+							  <Book
+								  book={book}
+								  changeShelf={this.props.changeShelf}
+								  currentShelf={this.props.shelfName}
+							  />
+						  </li>
+					  ))}
+				</ol>
+			  </div>
+			</div>
 		);
 	}
 }
-export default Shelf;
+export default Shelf
